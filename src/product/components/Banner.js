95a218c0084b5fd4085acf87c6a8cc1c87888e75
PreciousAma <preshchilaka06@gmail.com>
2022-08-23 00:00:06 +0100

    banner style updated

---
+++ src/product/components/Banner.js
@@ -0,0 +1,11 @@
+import { styled } from "@mui/material";
+import bannerImage from "../../images/image-hero-desktop.jpg";
+
+export const Banner = () => {
+  return <StyledBanner></StyledBanner>;
+};
+
+const StyledBanner = styled("div")({
+  backgroundImage: `url(${bannerImage})`,
+  paddingTop: "375px",
+});
--- src/product/pages/Product.js
+++ src/product/pages/Product.js
@@ -1,10 +1,12 @@
 import { Progress } from "../components/Progress";
 import { About } from "../components/About";
 import { TitleCard } from "../components/TitleCard";
+import { Banner } from "../components/Banner";
 
 export const Product = () => {
   return (
     <>
+      <Banner />
       <TitleCard />
       <Progress />
       <About />
